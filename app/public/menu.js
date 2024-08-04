f4d79702539a4e1e7eb604c58ac0a2466a4b7bef
UBC Student <joshuac2@pender.students.cs.ubc.ca>
2024-08-04 16:29:22 -0700

    changed cart data structure to a dictionary. Added subtotal tracking that adjusts when cart item is added/removed

--- app/public/menu.js
+++ app/public/menu.js
@@ -1,4 +1,4 @@
-let cart = [];
+let cart = {};
 let subtotal = 0;
 
 async function fetchAndDisplayMenu(restaurantAddress, restaurantName) {
@@ -26,33 +26,38 @@ async function fetchAndDisplayMenu(restaurantAddress, restaurantName) {
         const div = document.createElement('div');
         div.classList.add("menuItem");
         
-        div.onclick = () => addToCart(item[7]); // add an event listener to process menu item click
+        div.onclick = () => addToCart(item[7], item[10]); // add an event listener to process menu item click
         div.innerHTML = `<span>⊕ ${item[0]} -- ${item[7]}</span><span>$${item[10]}</span>`;
         menu_container.appendChild(div);
     });
 
-    const pTotal = document.createElement('p');
-    pTotal.innerText = `Subtotal: ${subtotal}`;
-    checkout_container.appendChild(pTotal);
+    // const cartContainer = document.getElementById("cart_container");
+    // cartContainer.innerHTML = '<h2>Cart</h2>'
+    // const pTotal = document.createElement('p');
+    // pTotal.innerText = `Subtotal: ${subtotal}`;
+    // checkout_container.appendChild(pTotal);
 
-    const checkoutButton = document.createElement('button');
-    checkoutButton.type = 'submit';
-    checkoutButton.classList.add('checkoutButton');
-    checkoutButton.textContent = 'Checkout';
-    checkout_container.appendChild(checkoutButton);
-    checkoutButton.addEventListener('submit', checkout);
+    // const checkoutButton = document.createElement('button');
+    // checkoutButton.type = 'submit';
+    // checkoutButton.classList.add('checkoutButton');
+    // checkoutButton.textContent = 'Checkout';
+    // checkout_container.appendChild(checkoutButton);
+    // checkoutButton.addEventListener('submit', checkout);
     
 }
 
-function addToCart(item) {
+function addToCart(item, price) {
     let add = confirm("Add this item to cart?");
     if (add) {
-        if (cart.includes(item)) {
+        if (item in cart) {
             alert("Item already in cart!");
             return;
         } else {
-            cart.push(item);
+            cart[item] = price;
+            subtotal+=cart[item];
+            subtotal = parseFloat(subtotal.toFixed(2)); // fixes float arithmetic issues
             alert(`Added ${item} to cart`);
+            console.log(subtotal);
             displayCart(); // 
         }
     }
@@ -68,7 +73,7 @@ function displayCart() {
 
         cartContainer.innerHTML = '<h2>Cart</h2>'
 
-        cart.forEach(item => {        
+        Object.keys(cart).forEach(item => {        
             const div = document.createElement('div');
             div.classList.add("cartItem");
             div.textContent = `⊖ ${item}`; // change div content to include item name and price?
@@ -76,14 +81,21 @@ function displayCart() {
 
             cartContainer.appendChild(div);
         });
+
+        const checkoutArea = document.getElementById("checkout_area");
+        checkoutArea.innerHTML = `
+            <button class = "checkoutButton"> CHECKOUT </button>
+            `;
 }
 
 function removeFromCart(item) {
     const remove = confirm(`Remove ${item} from cart?`);
     if (remove) {
-        const indexToRemove = cart.indexOf(item);
-        cart.splice(indexToRemove, 1);
+        subtotal-=cart[item];
+        subtotal = parseFloat(subtotal.toFixed(2)); // fixes float arithmetic issues
+        delete cart[item];
         displayCart(); // update cart on webpage
+        console.log(subtotal);
     }
 }
 
